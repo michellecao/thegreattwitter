5a94347723e6bb057688a9c0c5ff978e3f3cf288
Michelle Cao <michellecao@Michelles-MacBook-Pro-2.local>
2016-02-07 12:32:50 -0600

    add integration test

---
+++ TheGreatTwitter/src/integration-test/groovy/thegreattwitter/AccountIntegrationSpec.groovy
@@ -0,0 +1,22 @@
+package thegreattwitter
+
+
+import grails.test.mixin.integration.Integration
+import grails.transaction.*
+import spock.lang.*
+
+@Integration
+@Rollback
+class AccountIntegrationSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
